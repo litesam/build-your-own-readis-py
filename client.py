eb64a16d80e945eaf17c735b7d0ec31c7bc9eaa3
litesam <sam.anbu@zohocorp.com>
2023-02-11 15:33:17 +0530

    Cleaning up

--- client.py
+++ client.py
@@ -1,24 +1,24 @@
 import socket
 from typing import List
+from typing import Any
 
+# Always use list mutate the data wherever possible
 # Assume len to be len_
 HOST = "0.0.0.0"
 PORT = 1234
 k_max_msg = 4096
 
-def msg(m):
-    print("\033[1;31;40m", m, sep="")
 
-def query(fd: socket, text: List) -> int:
+def query(fd: socket.socket, text: List) -> int:
     len_ = len(text)
     if len_ > k_max_msg:
         return -1
-    
-    if err := write_all(fd, text, 4+len_):
-        return err
-    
+
+    if err := write_all(fd, text, len_):  # 4 is the length of the header, and the rest is the length
+        return int(err)
+
     # 4 bytes header
-    rbuf = ['' * (4 + k_max_msg + 1)]
+    rbuf = ['' * (k_max_msg + 1)]
     errno = 0
     err = read_full(fd, rbuf, 4)
     if err:
@@ -26,23 +26,25 @@ def query(fd: socket, text: List) -> int:
             print("EOF")
         else:
             print("read() error")
-        return err
-    
-    len_ = len(rbuf) # assume little endian
+        return int(err)
+
+    len_ = len(rbuf)  # assume little endian
     if len_ > k_max_msg:
         print("too long")
         return -1
-    
+
     # reply body
     err = read_full(fd, rbuf, len_)
     if err:
         print("read() error")
-        return err
+        return int(err)
 
     # do something
     print(f"server says: {rbuf}")
+    return 0
 
-def read_full(fd: socket, buf: str, n: int) -> int:
+
+def read_full(fd: socket.socket, buf: List, n: Any) -> int:
     while n > 0:
         rv = fd.recv(n)
         try:
@@ -51,43 +53,48 @@ def read_full(fd: socket, buf: str, n: int) -> int:
             pass
         if type(rv) is int and rv <= 0:
             return -1
-        
+
         try:
             n -= rv
         except:
+            buf += [str(rv)]
             pass
-        buf += str(rv)
     return 0
 
-def write_all(fd: socket, buf: str, n: int) -> int:
+
+def write_all(fd: socket.socket, buf: Any, n: int) -> int:
+    buf = ''.join(buf)
     buf = bytes(buf, 'utf-8')
     while n > 0:
         rv = fd.send(buf)
         if rv <= 0:
             return -1
-        
+
+        print(n)
         n -= rv
         rv = bytes(str(rv), 'utf-8')
         buf += rv
     return 0
+
 
 if __name__ == "__main__":
     fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     fd.connect((HOST, PORT))
 
     # multilple requests
-    err = query(fd, "hello1")
-    if err:
-        fd.close()
-        quit()
-    
-    err = query(fd, "hello2")
-    if err:
-        fd.close()
-        quit()
-    
-    err = query(fd, "hello3")
+    err = query(fd, list("hello1"))
     if err:
         fd.close()
         quit()
 
+    err = query(fd, list("hello2"))
+    if err:
+        fd.close()
+        quit()
+
+    err = query(fd, list("hello3"))
+    if err:
+        fd.close()
+        quit()
+
+    fd.close()
--- server.py
+++ server.py
@@ -1,23 +1,16 @@
 import socket
+from typing import List
+from typing import Any
 
 HOST = "0.0.0.0"
 PORT = 1234
 k_max_msg = 4096
 
-def msg(m):
-    print("\033[1;31;40m", m, sep="")
 
-def do_something(connfd):
-    rbuf = connfd.recv(64)
-    print(f"client says: {rbuf}")
-
-    wbuf = b"world"
-    connfd.send(wbuf)
-
-
-def one_request(connfd: socket):
+def one_request(connfd: socket.socket):
     # 4 bytes header
     rbuf = ['' * (4 + k_max_msg + 1)]
+    print('here1')
     errno = 0
     err = read_full(connfd, rbuf, 4)
     if err:
@@ -26,9 +19,10 @@ def one_request(connfd: socket):
         else:
             print("read() error")
         return err
-    
+
+    print('here2')
     len_ = len(rbuf)
-    if len_ > k_max_msg: # assume little endian
+    if len_ > k_max_msg:  # assume little endian
         print("too long")
         return -1
 
@@ -37,47 +31,50 @@ def one_request(connfd: socket):
     if err:
         print("read() error")
         return err
-    
+
+    print("here3")
     # do something
     # CPP sets the end position of the rbuf to '\0' escape sequence
     print(f"client says {rbuf}")
 
     # reply using the same protocol
     reply = "world"
-    print('here')
     len_ = len(reply)
     return write_all(connfd, reply, 4 + len_)
 
-def read_full(fd: socket, buf: str, n: int) -> int:
+
+def read_full(fd: socket.socket, buf: List, n: Any) -> int:
     while n > 0:
         rv = fd.recv(n)
+        print(f"here: {rv}")
         try:
             rv = int(rv)
         except:
             pass
         if type(rv) is int and rv <= 0:
             return -1
-        
+
         try:
             n -= rv
         except:
-            pass
-        buf += str(rv)
+            buf += [str(rv)]
     return 0
 
-def write_all(fd: socket, buf: str, n: int) -> int:
+
+def write_all(fd: socket.socket, buf: Any, n: int) -> int:
     while n > 0:
         rv = fd.send(buf)
         if rv <= 0:
             return -1
-        
+
         n -= rv
         buf += rv
     return 0
 
+
 if __name__ == "__main__":
-    fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    val = 1
+    fd: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    val: int = 1
     fd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, val)
     fd.bind((HOST, PORT))
     fd.listen()
@@ -91,4 +88,3 @@ if __name__ == "__main__":
             if err:
                 break
         connfd.close()
-
